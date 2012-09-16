46f370e13aeb69650e8b8f16286059a36481163d
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2012-09-16 02:31:01 +0100

    su640: Updated fingerprint and desc
    Change-Id: I4e292274abb5cb676f21f5e9095de638d901843d

--- cm.mk
+++ cm.mk
@@ -21,7 +21,7 @@ PRODUCT_VERSION_DEVICE_SPECIFIC :=
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := su640
 
-PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=lgp930 PRODUCT_NAME=lge_iproj BUILD_FINGERPRINT=LGE/i_skt/i_skt:2.3.5/GRJ90/LG-SU640-V10i.42481c03:user/release-keys PRIVATE_BUILD_DESC="i_skt-user 2.3.5 GRJ90 LG-SU640-V10i.42481c03 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=lge_iproj BUILD_FINGERPRINT=LGE/i_skt/i_skt:4.0.4/IMM76D/LG-SU640-V20d.1c14dd350a:user/release-keys PRIVATE_BUILD_DESC="i_skt-user 4.0.4 IMM76D LG-SU640-V20d.1c14dd350a release-keys"
 
 # Enable Torch
 PRODUCT_PACKAGES += Torch WiFiDirectDemo
