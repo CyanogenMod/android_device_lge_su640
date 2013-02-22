bcc82bef384b340c295b9baa92192b9c666ab031
Tom Marshall <tdm.code@gmail.com>
2013-02-22 15:02:45 +0000

    su640: Make init script and fstab model specific items.
    Model specific versions of init.iprj.rc, fstab.iprj
    
    Change-Id: I747023b1b57398bf8910378d965412da2d087004

--- su640.mk
+++ su640.mk
@@ -4,7 +4,12 @@ $(call inherit-product-if-exists, vendor/lge/su640/su640-vendor.mk)
 
 ## These are different between models
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/config/bcmdhd.cal:system/etc/wifi/bcmdhd.cal
+    $(LOCAL_PATH)/config/bcmdhd.cal:system/etc/wifi/bcmdhd.cal \
+    $(LOCAL_PATH)/fstab.iprj:root/fstab.iprj
+
+## Model specific files
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/init.iprj.rc:root/init.iprj.rc
 
 ## NFC 
 PRODUCT_COPY_FILES += \
